6621475b37d115f075f0f75de5d5cacf4a46826c
York Wang <yw544@nyu.edu>
2018-01-16 15:24:45 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -42,7 +42,7 @@ def selective_tweet_shortener(tweet)
 end
 
 def shortened_tweet_truncator(tweet)
-if word_substituter(tweet).length > 140
+if word_substituter(tweet).size > 140
   word_substituter[0...136] + "..."
 else
 tweet
